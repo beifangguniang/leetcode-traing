e29a95a009c7276797a28fdc4d907150036d8fd2
beifangguniang <44029445+beifangguniang@users.noreply.github.com>
2019-02-19 18:19:39 +0800

    Update code.c

--- Move zeroes/code.c
+++ Move zeroes/code.c
@@ -1,16 +1,15 @@
 void moveZeroes(int* nums, int numsSize) {
-    int z = 0;
-    for(int i =0; i< numsSize; i++)
-    {
-        if(nums[i]!=0)
-        {
-            nums[z]= nums[i];
-            z ++;
-        }
+    int tempSize = 0;  //目前已经加入多少个数字
+    //把nums中不是0的依序插入在temp阵列中
+    for (int i = 0; i < numsSize; i++ ){
+        if (nums[i] !=0){
+            nums[tempSize] = nums[i];
+            tempSize++;    
+        }  
     }
-    while(z<numsSize)
-    {
-        nums[z++] = 0;
+    // 将剩下的元素全部填成 0
+    while (tempSize < numsSize){
+        nums[tempSize] = 0;
+        tempSize++;
     }
-    return nums;
 }
