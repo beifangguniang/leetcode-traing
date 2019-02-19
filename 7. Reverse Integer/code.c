b03a76d070041bcf06b7ffa1e5f18dc72dcd6924
beifangguniang <44029445+beifangguniang@users.noreply.github.com>
2019-02-19 18:17:46 +0800

    Update code.c

--- 7. Reverse Integer/code.c
+++ 7. Reverse Integer/code.c
@@ -1,15 +1,15 @@
-void moveZeroes(int* nums, int numsSize) {
-    int tempSize = 0;  //目前已经加入多少个数字
-    //把nums中不是0的依序插入在temp阵列中
-    for (int i = 0; i < numsSize; i++ ){
-        if (nums[i] !=0){
-            nums[tempSize] = nums[i];
-            tempSize++;    
-        }  
-    }
-    // 将剩下的元素全部填成 0
-    while (tempSize < numsSize){
-        nums[tempSize] = 0;
-        tempSize++;
-    }
-}
+int reverse(int x) {
+	    int num = 0;
+	    while(x)
+	    {
+	        //记录num改变之前的值并用已改变的数来逆向推断是否相等，不等于说明是溢出了，返回0
+	        int temp = num*10 + x%10;
+	        if(temp/10 != num)       
+	        {
+	            return 0;
+	        }
+	        x = x/10;
+	        num = temp;
+	    }
+	    return num;
+	} 
